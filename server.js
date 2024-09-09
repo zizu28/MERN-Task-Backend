d4f032b20acfc21c3ebeee14042f84b4af410b35
Zulka <ziblimzulka.zz@gmail.com>
2024-09-09 13:25:10 +0700

    Resolved CORS

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config();
 const connectDB = require('./connect/database');
 const port = process.env.PORT || 5000;
+const Cors = require('cors')
 
 connectDB();
 const app = express();
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
   
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(Cors())
 
 app.use('/api/tasks', require('./routes/taskRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
